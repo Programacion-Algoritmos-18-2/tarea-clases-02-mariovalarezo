af32f541b665ec085b261949e15e5e6710a60547
Mario Valarezo <madx1029@gmail.com>
2018-10-12 10:25:45 -0500

    Ejercicio 2

---
+++ Ejercicio2/src/ejercicio2/Precio.java
@@ -0,0 +1,28 @@
+
+package ejercicio2;
+
+/**
+ *
+ * @author Mario
+ */
+public class Precio {
+    double euros;
+    
+    public double obtener(){
+        
+        return 0;
+    }
+public void agregar(double x){
+    
+}
+   
+    
+}
+ class Principal{
+    public static void main(String[] args) {
+        Precio p =  new Precio();
+        System.out.println(p.euros);
+        
+    }
+    
+}
