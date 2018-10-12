f59330b548d6693c9cc0e772b58f4dce763e543e
Mario Valarezo <madx1029@gmail.com>
2018-10-12 11:17:07 -0500

    Ejercicios semana 2

--- Ejercicio2/src/Paquete3/Precio.java
+++ Ejercicio2/src/Paquete3/Precio.java
@@ -19,6 +19,10 @@ public class Precio {
     public Precio(){
         agregar(30.2);
     }
+    
+    public Precio(double y){
+        agregar(y);
+    }
 
 }
 
@@ -26,12 +30,15 @@ class Principal {
 
     public static void main(String[] args) {
         Precio p = new Precio();
+        Precio n = new Precio(25.9);
         /*System.out.println(p.euros);
         p.euros = 10.1;
         System.out.println(p.euros);
         
          p.agregar(20.2);*/
+        
          System.out.println(p.obtener());
+         System.out.println(n.obtener());
 
     }
 
